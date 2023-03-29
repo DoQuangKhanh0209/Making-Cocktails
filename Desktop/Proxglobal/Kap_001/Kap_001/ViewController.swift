c00f1525e5ecb9863a8326d0a947c556072e129e
DoQuangKhanh0209 <vuagameboy99@gmail.com>
2023-03-29 14:39:43 +0700

    a

---
+++ Desktop/Proxglobal/Kap_001/Kap_001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kap_001
+//
+//  Created by Quang Khánh on 29/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
